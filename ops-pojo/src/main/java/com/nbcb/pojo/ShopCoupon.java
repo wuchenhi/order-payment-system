30696c5e918294cf24be3dc293e74ab101f10edd
wuchenhi <22125194@zju.edu.cn>
2023-07-25 13:52:26 +0800

    @Data

--- ops-pojo/src/main/java/com/nbcb/pojo/ShopCoupon.java
+++ ops-pojo/src/main/java/com/nbcb/pojo/ShopCoupon.java
@@ -1,9 +1,12 @@
 package com.nbcb.pojo;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
 public class ShopCoupon implements Serializable {
     private Long couponId;
 
@@ -17,51 +20,51 @@ public class ShopCoupon implements Serializable {
 
     private Date usedTime;
 
-    public Long getCouponId() {
-        return couponId;
-    }
-
-    public void setCouponId(Long couponId) {
-        this.couponId = couponId;
-    }
-
-    public BigDecimal getCouponPrice() {
-        return couponPrice;
-    }
-
-    public void setCouponPrice(BigDecimal couponPrice) {
-        this.couponPrice = couponPrice;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Integer getIsUsed() {
-        return isUsed;
-    }
-
-    public void setIsUsed(Integer isUsed) {
-        this.isUsed = isUsed;
-    }
-
-    public Date getUsedTime() {
-        return usedTime;
-    }
-
-    public void setUsedTime(Date usedTime) {
-        this.usedTime = usedTime;
-    }
+//    public Long getCouponId() {
+//        return couponId;
+//    }
+//
+//    public void setCouponId(Long couponId) {
+//        this.couponId = couponId;
+//    }
+//
+//    public BigDecimal getCouponPrice() {
+//        return couponPrice;
+//    }
+//
+//    public void setCouponPrice(BigDecimal couponPrice) {
+//        this.couponPrice = couponPrice;
+//    }
+//
+//    public Long getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(Long userId) {
+//        this.userId = userId;
+//    }
+//
+//    public Long getOrderId() {
+//        return orderId;
+//    }
+//
+//    public void setOrderId(Long orderId) {
+//        this.orderId = orderId;
+//    }
+//
+//    public Integer getIsUsed() {
+//        return isUsed;
+//    }
+//
+//    public void setIsUsed(Integer isUsed) {
+//        this.isUsed = isUsed;
+//    }
+//
+//    public Date getUsedTime() {
+//        return usedTime;
+//    }
+//
+//    public void setUsedTime(Date usedTime) {
+//        this.usedTime = usedTime;
+//    }
 }
--- ops-pojo/src/main/java/com/nbcb/pojo/ShopGoods.java
+++ ops-pojo/src/main/java/com/nbcb/pojo/ShopGoods.java
@@ -1,9 +1,11 @@
 package com.nbcb.pojo;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
-
+@Data
 public class ShopGoods implements Serializable {
 
     private Long goodsId;
@@ -18,51 +20,51 @@ public class ShopGoods implements Serializable {
 
     private Date addTime;
 
-    public Long getGoodsId() {
-        return goodsId;
-    }
-
-    public void setGoodsId(Long goodsId) {
-        this.goodsId = goodsId;
-    }
-
-    public String getGoodsName() {
-        return goodsName;
-    }
-
-    public void setGoodsName(String goodsName) {
-        this.goodsName = goodsName == null ? null : goodsName.trim();
-    }
-
-    public Integer getGoodsNumber() {
-        return goodsNumber;
-    }
-
-    public void setGoodsNumber(Integer goodsNumber) {
-        this.goodsNumber = goodsNumber;
-    }
-
-    public BigDecimal getGoodsPrice() {
-        return goodsPrice;
-    }
-
-    public void setGoodsPrice(BigDecimal goodsPrice) {
-        this.goodsPrice = goodsPrice;
-    }
-
-    public String getGoodsDesc() {
-        return goodsDesc;
-    }
-
-    public void setGoodsDesc(String goodsDesc) {
-        this.goodsDesc = goodsDesc == null ? null : goodsDesc.trim();
-    }
-
-    public Date getAddTime() {
-        return addTime;
-    }
-
-    public void setAddTime(Date addTime) {
-        this.addTime = addTime;
-    }
+//    public Long getGoodsId() {
+//        return goodsId;
+//    }
+//
+//    public void setGoodsId(Long goodsId) {
+//        this.goodsId = goodsId;
+//    }
+//
+//    public String getGoodsName() {
+//        return goodsName;
+//    }
+//
+//    public void setGoodsName(String goodsName) {
+//        this.goodsName = goodsName == null ? null : goodsName.trim();
+//    }
+//
+//    public Integer getGoodsNumber() {
+//        return goodsNumber;
+//    }
+//
+//    public void setGoodsNumber(Integer goodsNumber) {
+//        this.goodsNumber = goodsNumber;
+//    }
+//
+//    public BigDecimal getGoodsPrice() {
+//        return goodsPrice;
+//    }
+//
+//    public void setGoodsPrice(BigDecimal goodsPrice) {
+//        this.goodsPrice = goodsPrice;
+//    }
+//
+//    public String getGoodsDesc() {
+//        return goodsDesc;
+//    }
+//
+//    public void setGoodsDesc(String goodsDesc) {
+//        this.goodsDesc = goodsDesc == null ? null : goodsDesc.trim();
+//    }
+//
+//    public Date getAddTime() {
+//        return addTime;
+//    }
+//
+//    public void setAddTime(Date addTime) {
+//        this.addTime = addTime;
+//    }
 }
